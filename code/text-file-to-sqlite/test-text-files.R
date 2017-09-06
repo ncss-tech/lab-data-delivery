832740c2572a908d3382ed3cc1ac2301d2d1ef34
Dylan Beaudette <dylan.beaudette@gmail.com>
2017-09-06 11:10:21 -0700

    adding some files to help text to SQLite conversion

---
+++ code/text-file-to-sqlite/test-text-files.R
@@ -0,0 +1,25 @@
+##
+##
+##
+
+library(readr)
+library(plyr)
+
+# files to check
+base.path <- 'C:/Users/Dylan.Beaudette/Desktop/NASIS-LIMS-dump/NASIS_Pedons/CSV_files/'
+files <- list.files(path=base.path, pattern='\\\\*.csv')
+
+# init list to store parsing problems
+l <- list()
+
+# iterate over files
+for(i in files) {
+  this.file <- paste0(base.path, i)
+  x <- read_delim(this.file, delim = '|', quote='', na='', comment='', trim_ws = TRUE, guess_max = 1e6)
+  l[[i]] <- problems(x)
+}
+
+# flatten
+d <- ldply(l)
+
+# summarize
