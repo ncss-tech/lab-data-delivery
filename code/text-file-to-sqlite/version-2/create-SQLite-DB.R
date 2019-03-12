e68ae4c3bea497240833bbfbe48c63a027015c36
Dylan Beaudette <debeaudette@ucdavis.edu>
2019-03-12 11:24:47 -0700

    adding indexs

--- code/text-file-to-sqlite/version-2/create-SQLite-DB.R
+++ code/text-file-to-sqlite/version-2/create-SQLite-DB.R
@@ -9,14 +9,11 @@ writeTable <- function(f, table.name) {
   # seems to work well
   x <- read_delim(f, delim = '|')
   
-  ## TODO: check these
-  # p <- problems(x)
+  # save problems for later review
+  p <- as.data.frame(problems(x))
   
+  # convert to DF
   x <- as.data.frame(x)
-  
-  ## TODO:
-  # fix column types: all NULL --> columns appear as logical 
-  # which(sapply(x, is.logical))
   
   # write table
   dbWriteTable(db, name = table.name, value = x, row.names=FALSE, overwrite=TRUE)
@@ -25,38 +22,39 @@ writeTable <- function(f, table.name) {
   schema.file <- sprintf("%s-schema.sql", table.name)
   cat(sqliteBuildTableDefinition(db, table.name, value=x[0,], row.names=FALSE), file = schema.file, sep = '\n')
   
+  return(p)
 }
 
 
 
 # note, finds DB handle from global environment
-indexTable <- function(tbl, field = c('labsampnum', 'result_source_key')) {
+indexTable <- function(tbl, field = c('labsampnum', 'result_source_key', 'prep_code')) {
   index.query <- sprintf("CREATE INDEX IF NOT EXISTS %s_%s_idx ON %s (%s);", tbl, field, tbl, field)
   sapply(index.query, dbExecute, conn=db)
 }
 
 
 # output file
-db.file <- 'LDM-compact.sqlite'
+db.file <- 'E:/NASIS-KSSL-LDM/LDM/LDM-compact.sqlite'
 
 # init DB
 # best to remove old version first
 unlink(db.file)
 db <- dbConnect(RSQLite::SQLite(), db.file)
 
-writeTable('EXPORT_Calculations_Including_Estimates_And_Default_Values_20190306.txt.gz', table.name = 'calculations')
+calculations.p <- writeTable('EXPORT_Calculations_Including_Estimates_And_Default_Values_20190306.txt.gz', table.name = 'calculations')
 
-writeTable('EXPORT_Chemical_Properties_20190306.txt.gz', table.name = 'chemical')
+chemical.p <- writeTable('EXPORT_Chemical_Properties_20190306.txt.gz', table.name = 'chemical')
 
-writeTable('EXPORT_combine_nasis_ncss_20190306.txt.gz', table.name = 'ncss_site')
+ncss_site.p <- writeTable('EXPORT_combine_nasis_ncss_20190306.txt.gz', table.name = 'ncss_site')
 
-writeTable('EXPORT_Major_And_Trace_Elements_And_Oxides_20190306.txt.gz', table.name = 'geochemical')
+geochemical.p <- writeTable('EXPORT_Major_And_Trace_Elements_And_Oxides_20190306.txt.gz', table.name = 'geochemical')
 
-writeTable('EXPORT_Mineralogy_Glass_Count_20190306.txt.gz', table.name = 'glass')
+glass.p <- writeTable('EXPORT_Mineralogy_Glass_Count_20190306.txt.gz', table.name = 'glass')
 
-writeTable('EXPORT_Physical_Properties_20190306.txt.gz', table.name = 'physical')
+physical.p <- writeTable('EXPORT_Physical_Properties_20190306.txt.gz', table.name = 'physical')
 
-writeTable('EXPORT_XRay_And_Thermal_20190306.txt.gz', table.name = 'xray_thermal')
+xray_thermal.p <- writeTable('EXPORT_XRay_And_Thermal_20190306.txt.gz', table.name = 'xray_thermal')
 
 # check
 dbListTables(db)
@@ -81,10 +79,10 @@ indexTable('ncss_site', 'pedlabsampnum')
 # NASIS -> lab: pedlabsampnum + labsampnum 
 
 
-# check
-dbGetQuery(db, "SELECT * from ncss_site WHERE upedonid = 'S1999NY061001' ;")
-dbGetQuery(db, "SELECT * from physical WHERE result_source_key = 1 ;")
-dbGetQuery(db, "SELECT * from chemical WHERE result_source_key = 1 ;")
+# get data
+dbGetQuery(db, "SELECT pedon_key, pedlabsampnum, pedoniid, upedonid, corr_name from ncss_site WHERE upedonid = 'S1999NY061001' ;")
+dbGetQuery(db, "SELECT result_source_key, prep_code, labsampnum, clay_total, particle_size_method from physical WHERE result_source_key = 1 ;")
+dbGetQuery(db, "SELECT result_source_key, prep_code, labsampnum, ca_nh4_ph_7, ca_nh4_ph_7_method from chemical WHERE result_source_key = 1 ;")
 
 # cleanup
 dbExecute(db, 'VACUUM;')
