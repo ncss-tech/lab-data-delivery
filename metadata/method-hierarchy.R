b18869de979e0371849a9e91df11e36d753009d9
Dylan Beaudette <debeaudette@ucdavis.edu>
2020-11-05 12:10:59 -0800

    cleaner output and SDA example

--- metadata/method-hierarchy.R
+++ metadata/method-hierarchy.R
@@ -3,15 +3,18 @@ library(RSQLite)
 library(soilDB)
 library(data.tree)
 library(stringr)
+library(soilDB)
 
 
 ## TODO: 
-# clean leading white space in method.requested_anal_name
+# clean leading white space in method.requested_anal_name in source data
 # where does the source data come from, static | dynamic?
 # make this into an interactive HTML widget with links to the SSIR 42PDF
 # integrate with a more fully parsed (Andrew?) version of SSIR 42
 
-
+## Notes:
+# you can get this same information from SDA by querying the "lab_methods" table
+# 
 
 
 # connect
@@ -22,6 +25,10 @@ x <- dbGetQuery(db, qq)
 
 # done with connection
 dbDisconnect(db)
+
+
+# # alternatively do it with SDA
+# x <- SDA_query("SELECT DISTINCT procedure_key, proced_abbrev, requested_anal_name, proced_name, proced_code, proced_desc FROM lab_method_code ;") 
 
 
 # clean whitespace
@@ -40,12 +47,53 @@ x$path <- sprintf("%s|%s|%s|%s|%s", 'NCSS Lab Methods', x$method_group, x$reques
 res <- as.Node(x, pathName = 'path', pathDelimiter = '|', 'procedure_key', 'proced_abbrev')
 
 
-# demonstrate output for a single group of methods
+# there is redundancy in this tree
+# related to a single method within a method group
 res$Sulfur
+
+# explicit copy for testing
+res2 <- Clone(res)
+
+# z: 2nd-level node
+simplifyMethodTree <- function(z) {
+  
+  # get child nodes and details
+  z.children <- z$children
+  n <- length(z.children)
+  nm <- names(z.children)
+  
+  if(n < 2) {
+    # remove all children from parent node
+    orphans <- z$RemoveChild(nm)
+    
+    # add the child nodes to the main DT
+    res2$AddChildNode(orphans)
+    # remove this node 
+    res2$RemoveChild(z$name)
+  }
+  
+  # otherwise do nothing
+}
+
+
+# operate on 2nd level (method groups) only
+res2$Do(simplifyMethodTree, filterFun = function(i) i$level == 2)
+
+# did that work as expected?
+res$Sulfur
+res2$`Sulfur, Total`
+
+res$`Bulk Density`
+res2$`Bulk Density`
+
+res$PSDA
+res2$PSDA
+
+# I think so, still need to check
 
 
 # save to text files for review
 options(width = 250)
 sink(file = 'all-methods.txt')
-print(res, pruneMethod = NULL)
+print(res2, pruneMethod = NULL)
 sink()
