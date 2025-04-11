a1b5f2a106611b75cea19bbd3913055aada759a1
Stephen Roecker <stephen.roecker@usda.gov>
2025-04-11 16:42:25 -0500

    exporting dm as sql
    can be imported into dbdiagram.io

--- standard/data-model.R
+++ standard/data-model.R
@@ -52,12 +52,13 @@ scd_dm <- scd_dm |>
   dm_add_pk(labanalyte,           columns = analytekey) |>
   dm_add_pk(labarea,              columns = areakey) |> # missing from NASIS
   # dm_add_pk(labcombinenasisncss,  columns = pedlabsampnum) |>
-  dm_add_pk(lablayer,             columns = c(layerkey,
-                                              labsampnum
+  dm_add_pk(lablayer,             columns = c(layerkey #,
+                                              # labsampnum
                                               )) |>
   dm_add_pk(labmethodcode,        columns = mcid) |>
-  dm_add_pk(labpedon,             columns = c(pedonkey,
-                                              pedlabsampnum)) |>
+  dm_add_pk(labpedon,             columns = c(pedonkey #,
+                                              # pedlabsampnum
+                                              )) |>
   dm_add_pk(labpreparation,       columns = prepkey, autoincrement = TRUE) |>
   dm_add_pk(labsite,              columns = sitekey, autoincrement = TRUE) |>
   dm_add_pk(labmir,               columns = id) |> # missing from NASIS
@@ -77,21 +78,26 @@ scd_dm <- scd_dm |>
 
   # foreign keys ----
 scd_dm <- scd_dm |>
-    dm_add_fk(labcalculationsandestimates, columns = c(layerkey,
-                                                       labsampnum),
-              ref_table = lablayer,        ref_columns = c(layerkey,
-                                                           labsampnum)) |>
-    dm_add_fk(labchemicalproperties,       columns = c(layerkey,
-                                                       labsampnum),
-              ref_table = lablayer,        ref_columns = c(layerkey,
-                                                           labsampnum)) |>
-    dm_add_fk(labcombinenasisncss,         columns = c(pedonkey,
-                                                       pedlabsampnum
+    dm_add_fk(labcalculationsandestimates, columns = c(layerkey #,
+                                                       # labsampnum
+                                                       ),
+              ref_table = lablayer,        ref_columns = c(layerkey #,
+                                                           # labsampnum
+                                                           )) |>
+    dm_add_fk(labchemicalproperties,       columns = c(layerkey #,
+                                                       #absampnum
+                                                       ),
+              ref_table = lablayer,        ref_columns = c(layerkey #,
+                                                           #labsampnum
+                                                           )) |>
+    dm_add_fk(labcombinenasisncss,         columns = c(pedonkey #,
+                                                       #pedlabsampnum
                                                        # sitekey,
                                                        # peiid
                                                        ),
-              ref_table = labpedon,        ref_columns = c(pedonkey,
-                                                           pedlabsampnum)) |>
+              ref_table = labpedon,        ref_columns = c(pedonkey #,
+                                                           #pedlabsampnum
+                                                           )) |>
     dm_add_fk(labcombinenasisncss,         columns = sitekey,
               ref_table = labsite,         ref_columns = sitekey) |>
     dm_add_fk(lablayer,                    columns = c(pedonkey,
@@ -99,31 +105,37 @@ scd_dm <- scd_dm |>
                                                      # sitekey,
                                                      ),
             ref_table = labpedon,        ref_columns = pedonkey) |>
-    dm_add_fk(labphysicalproperties,       columns = c(layerkey,
-                                                       labsampnum),
-              ref_table = lablayer,      ref_columns = c(layerkey,
-                                                         labsampnum)) |>
+    dm_add_fk(labphysicalproperties,       columns = c(layerkey #,
+                                                       #labsampnum
+                                                       ),
+              ref_table = lablayer,      ref_columns = c(layerkey #,
+                                                         #labsampnum
+                                                         )) |>
   # dm_add_pk(labmajortrelementsoxides,    columns = layerkey) |>
-    dm_add_fk(labmajortrelementsoxides,    columns = c(layerkey,
-                                                       labsampnum
+    dm_add_fk(labmajortrelementsoxides,    columns = c(layerkey #,
+                                                       #labsampnum
                                                        # resourcesourcekey
                                                        ),
-              ref_table = lablayer,        ref_columns = c(layerkey,
-                                                         labsampnum)) |>
-  dm_add_fk(labmineralogyglasscount,     columns = c(layerkey,
-                                                     labsampnum
+              ref_table = lablayer,        ref_columns = c(layerkey #,
+                                                           #labsampnum
+                                                           )) |>
+  dm_add_fk(labmineralogyglasscount,     columns = c(layerkey #,
+                                                     #labsampnum
                                                      # resultsourcekey
                                                      ),
-            ref_table = lablayer,        ref_columns = c(layerkey,
-                                                     labsampnum)) |>
+            ref_table = lablayer,        ref_columns = c(layerkey #,
+                                                         #labsampnum
+                                                         )) |>
   # dm_add_fk(labmethodcode,               columns = c(procedurekey,
   #                                                    # sourcesystemkey
   #                                                    ),
   # ref_table = labanalysisprocedure, ref_columns = c(procedurekey)) |>
-  dm_add_fk(labmir,                      columns = c(layerkey,
-                                                     labsampnum),
-            ref_table = lablayer,        ref_columns = c(layerkey,
-                                                       labsampnum)) |>
+  dm_add_fk(labmir,                      columns = c(layerkey #,
+                                                     #labsampnum
+                                                     ),
+            ref_table = lablayer,        ref_columns = c(layerkey #,
+                                                         #labsampnum
+                                                         )) |>
   dm_add_fk(labmir,                       columns     = dwavelengtharrayid,
             ref_table = labmirwavelength, ref_columns = dwavelengtharrayid) |>
   dm_add_fk(labpedon,                    columns = sitekey,
@@ -134,12 +146,13 @@ scd_dm <- scd_dm |>
             ref_table = lablayer,        ref_columns = layerkey) |>
   # dm_add_fk(labsite,                     columns = c(countykey),
   #           ref_table = labarea,         ref_columns = c(countykey = areakey)) |>
-  dm_add_fk(labxrayandthermal,           columns = c(layerkey,
-                                                     labsampnum #,
+  dm_add_fk(labxrayandthermal,           columns = c(layerkey #,
+                                                     # labsampnum #,
                                                      # resultsourcekey
                                                      ),
-            ref_table = lablayer,        ref_column = c(layerkey,
-                                                        labsampnum)) |>
+            ref_table = lablayer,        ref_column = c(layerkey #,
+                                                        #labsampnum
+                                                        )) |>
   dm_add_fk(labwebmap,                   columns = c(pedonkey,
                                                      # peiid
                                                      ),
@@ -163,6 +176,7 @@ scd_dm <- scd_dm |>
 
   
   
+dm_draw(scd_dm, column_types = TRUE)
 test <- dm_draw(scd_dm)
 test |> 
   DiagrammeRsvg::export_svg() |>
@@ -172,16 +186,20 @@ test |> htmltools::HTML() |> htmltools::html_print()
 
 scd_dm2 <- dm_select_tbl(scd_dm, labanalysisprocedure, labsite, labpreparation)
 
-destination_db <- DBI::dbConnect(RSQLite::SQLite(), bigint = "integer64", dbname = "ncss_labdata_relationships6.sqlite")
-scd_dm <- copy_dm_to(
+
+destination_db <- DBI::dbConnect(RSQLite::SQLite(), bigint = "integer64", dbname = "ncss_labdata_relationships.sqlite")
+copy_dm_to(
   destination_db, 
   scd_dm, 
   temporary = FALSE,
   set_key_constraints = TRUE
   )
 
-fp <- "C:/workspace2/test10.sqlite"
-dm_con <- dm_from_con(DBI::dbConnect(RSQLite::SQLite(), dbname = fp))
-dm_con
+template_db <- DBI::dbConnect(RSQLite::SQLite(), bigint = "integer64", dbname = "ncss-scd_template_db.sqlite")
 
+tmp1 <- dm_ddl_pre(scd_dm, template_db, temporary = FALSE)
+tmp2 <- dm_ddl_post(scd_dm, template_db, temporary = FALSE)
+
+lapply(tmp1, write, "test.txt", append = TRUE)
+lapply(tmp2$indexes, write, "test2.txt", append = TRUE)
 
